e7b2a0c8e682b8c32afc14d4a8037e7e2baef1e7
Mohit Singh <mohitpratap5712@gmail.com>
2025-09-06 12:09:57 +0530

    new js added

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle('active')
     close.classList.toggle('active')
 })
+
+let parent = document.querySelector(".functionimg")
+let child = document.querySelectorAll(".childslide")
+let index = 0 ;
+
+function slidefunc(){
+    index++;
+    if (index>=child.length)
+    {
+        index = 0;
+    }
+    parent.scrollTo({
+        left : child[index].offsetLeft,
+        behavior:"smooth"
+
+    })
+}
+setInterval(slidefunc,2000);
